aaa2c6f07556d0a98c7fe0a94601ef7308b753ba
perswalus <114963798+perswalus@users.noreply.github.com>
2023-11-10 10:53:33 -0500

    52

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-let button= document.querySelector('button');
+let button = document.querySelector('button');
 
-
-  button.onclick = {};
-  let  = Math.floor(Math.random() * 100) + 1;
-  let guesses = [];
-
-  function checkGuess() {
-    const userGuess = parseInt(document.getElementById('userGuess').value);
-    if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
-      alert('Please enter a valid number between 1 and 100.');
-      return;
-    }
-
-    const difference = Math.abs(solutionNumber - userGuess);
-    let temperature = getTemperature(difference);
-
-    guesses.push({ guess: userGuess, temperature: temperature });
-    displayGuesses();
-    if (userGuess === solutionNumber) {
-      document.getElementById('feedback').innerHTML = `<p>Congratulations! You guessed the correct number: ${solutionNumber}!</p>`;
-    }
+button.onclick = function () {
+  checkGuess();
+;
+let solutionNumber = Math.floor(Math.random() * 100) + 1;
+let guesses = [];
+function checkGuess() {
+ 
+  const userGuess = (document.getElementById("#check").guesses);
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+    alert('Please enter a valid number between 1 and 100.');
+    return;
   }
 
-  function getTemperature(difference) {
-    if (difference === 0) {
-      return 'You got it! ';
-    } else if (difference <= 5) {
-      return 'Very hot';
-    } else if (difference <= 10) {
-      return 'Hot';
-    } else if (difference <= 20) {
-      return 'Warm';
-    } else if (difference <= 30) {
-      return 'Cold';
-    } else {
-      return 'Very cold';
-    }
+  const difference = Math.abs(solutionNumber - userGuess);
+  let temperature = getTemperature(difference);
+
+  userGuess.onclick({ guess: userGuess, temperature: temperature });
+  displayGuesses();
+  if (userGuess === solutionNumber) {
+    document.getElementById('feedback').innerHTML = `<p>Congratulations! You guessed the correct number: ${solutionNumber}!</p>`;
+    return
   }
+}
 
-  function displayGuesses() {
-    let log = '<p>Your guesses:</p>';
-    guesses.forEach((item) => {
-      log += `<p>You guessed ${item.guess} - ${item.temperature}</p>`;
-    });
-    document.getElementById('feedback').innerHTML = log;
+function getTemperature(difference) {
+  if (difference === 26) {
+    return 'You got it! ';
+  } else if (difference <= 5) {
+    return 'Very hot';
+  } else if (difference <= 10) {
+    return 'Hot';
+  } else if (difference <= 20) {
+    return 'Warm';
+  } else if (difference <= 30) {
+    return 'Cold';
+  } else {
+    return 'Very cold';
   }
+}
 
-
+function displayGuesses() {
+  let log = '<p>Your guesses:</p>';
+  userGuess.forEach((item) => {
+    log += `<p>You guessed ${item.guess} - ${item.temperature}</p>`;
+  });
+  document.getElementById('feedback').innerHTML = log;
+  return
+};
+};
 
 
